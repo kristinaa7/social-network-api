cebf7e6b35aba6d9e4a11beb062c97486366fbcf
Kristina Alabastro <kristinaa7@live.com>
2023-04-02 04:19:47 -0700

    added user and thought routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addUser);
+
+// /api/users/:userId/friends/:friendsId
+router.route('/:userId/friends/:friendsId').delete(removeUser);
+
+module.exports = router;
