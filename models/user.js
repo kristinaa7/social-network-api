703fe5cc6fb68c73d3fa46bca3f5d68ea5128607
Kristina Alabastro <kristinaa7@live.com>
2023-04-02 04:49:59 -0700

    removed full nameproperty

--- models/user.js
+++ models/user.js
@@ -28,20 +28,6 @@ const userSchema = new Schema(
   }
 );
 
-// Create a virtual property `fullName` that gets and sets the user's full name
-userSchema
-  .virtual('fullName')
-  // Getter
-  .get(function () {
-    return `${this.first} ${this.last}`;
-  })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
-  });
-
 // Initialize our User model
 const User = model('user', userSchema);
 
