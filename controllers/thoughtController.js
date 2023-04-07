4df78a8b1b720e588d352b8f3f70507ca925fa19
Kristina Alabastro <kristinaa7@live.com>
2023-04-07 12:47:21 -0700

    moved reaction schema into new file

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -29,12 +29,19 @@ module.exports = {
   },
   // Delete a thought
   deleteThought(req, res) {
+    console.log('delete id =', req.params.thoughtId)
     Thought.findOneAndDelete({ _id: req.params.thoughtId })
-      .then((thought) =>
+      .then((thought) => {
+        console.log('delete succeded, thought =', thought)
         !thought
           ? res.status(404).json({ message: 'No thought with that ID' })
-          : User.deleteMany({ _id: { $in: thought.students } })
+          : 
+            User.findOneAndUpdate(
+              {thoughts: req.params.thoughtId},
+              {$pull: {thoughts: req.params.thoughtId}},
+              {new: true}
       )
+        })
       .then(() => res.json({ message: 'Thought deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
@@ -50,6 +57,37 @@ module.exports = {
           ? res.status(404).json({ message: 'No thought with this id!' })
           : res.json(thought)
       )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Create a reaction
+  createReaction(req, res) {
+    console.log('thoughts id = ', req.params.thoughtId)
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId }, 
+      { $addToSet: {reactions: req.body}},
+      { runValidators: true, new: true,}
+      )
+      .then((thought) => { 
+        console.log('thought =', thought)
+        res.json(thought)})
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Delete a reaction
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: {reactions: {reactionId: req.params.reactionId}}},
+      { runValidators: true, new: true,}
+      )
+      .then((thought) =>{
+      if (  !thought) {
+          res.status(404).json({ message: 'No reaction with that ID' })
+  }})
+      .then(() => res.json( thought ))
       .catch((err) => res.status(500).json(err));
   },
 };
---
+++ models/reaction.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment')
+
+const reactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+      type: String,
+      required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now(),
+        get: createdAtDate => moment(createdAtDate).format('MMM DD, YYYY [at] hh:mm a')
+      },
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+
+//Initialize our post model
+const Reaction = model('Reaction', reactionSchema);
+
+module.exports = Reaction
